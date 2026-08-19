a07fc8295f8ce7964060c4d5a31d4194a7fafc44
Thomas Rosen <thomas.rosen@me.com>
2026-08-20 00:15:50 +0200

    init storybook

---
+++ .storybook/main.js
@@ -0,0 +1,18 @@
+
+
+/** @type { import('@storybook/react-vite').StorybookConfig } */
+const config = {
+  "stories": [
+    "../stories/**/*.mdx",
+    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  "addons": [
+    "@chromatic-com/storybook",
+    "@storybook/addon-vitest",
+    "@storybook/addon-a11y",
+    "@storybook/addon-docs",
+    "@storybook/addon-onboarding"
+  ],
+  "framework": "@storybook/react-vite"
+};
+export default config;
